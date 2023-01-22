6e955e8d9460114b1c3ba43e650c14c42782cea7
jaepang <jake_king@kakao.com>
2023-01-22 23:00:42 +0900

    add fallback to article page

--- pages/article/[pageId]/index.tsx
+++ pages/article/[pageId]/index.tsx
@@ -1,9 +1,17 @@
 import Layout from '@components/Layout'
 import ArticlePageComponent from '@components/pages/article/ArticlePageComponent'
 
+import { useRouter } from 'next/router'
+
 export { getStaticPaths, getStaticProps } from './getStatics'
 
 export default function ArticlePage({ title, blocks }) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
   return (
     <Layout>
       <ArticlePageComponent {...{ title, blocks }} />
