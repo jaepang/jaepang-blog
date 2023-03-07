2708d77ad010742cf8f6fb326de60b2b9d167758
jaepang <jake_king@kakao.com>
2023-03-07 13:14:29 +0900

    image zoom implemented

--- app/(service)/(_components)/notion/Image/index.tsx
+++ app/(service)/(_components)/notion/Image/index.tsx
@@ -1,17 +1,74 @@
 'use client'
 
+import { useState, useEffect } from 'react'
+import { useWindowSize } from '@hooks/useWindowSize'
+
 import classNames from 'classnames/bind'
 import styles from './Image.module.css'
 const cx = classNames.bind(styles)
 
+function calcCenterZoomTransform(
+  width: number,
+  height: number,
+  imgProperty: { width: number; height: number; x: number; y: number },
+) {
+  const scale = Math.min(width / imgProperty.width, height / imgProperty.height)
+  const translateY =
+    -imgProperty.y +
+    (imgProperty.width <= imgProperty.height
+      ? (imgProperty.height * (scale - 1)) / 2
+      : (height - imgProperty.height) / 2)
+
+  return `translateY(${translateY}px) scale(${scale})`
+}
+
 export default function ImageBlock({ id, src, caption }) {
+  const [isZoomed, setIsZoomed] = useState(false)
+  const [imgProperty, setImgProperty] = useState({
+    width: 1,
+    height: 1,
+    x: 0,
+    y: 0,
+  })
+  const { width, height } = useWindowSize()
+  const transform = calcCenterZoomTransform(width, height, imgProperty)
+
+  useEffect(() => {
+    if (window) {
+      window.onscroll = () => setIsZoomed(false)
+    }
+  })
+
+  useEffect(() => {
+    if (document) {
+      setImgProperty(document?.getElementById(id)?.getBoundingClientRect() ?? { width: 1, height: 1, x: 0, y: 0 })
+    }
+  }, [id])
+
+  function handleClick() {
+    if (document) {
+      setImgProperty(document?.getElementById(id)?.getBoundingClientRect() ?? { width: 1, height: 1, x: 0, y: 0 })
+    }
+    setIsZoomed(prev => !prev)
+  }
+
   return (
-    <div className={cx('root')}>
-      <figure className={cx('figure')}>
-        {/** TODO: implement zoom */}
-        <img id={id} className={cx('image')} src={src} alt={caption} />
-        {caption && <figcaption>{caption}</figcaption>}
-      </figure>
-    </div>
+    <>
+      <div className={cx('root', { zoom: isZoomed })} onClick={handleClick}>
+        <figure className={cx('figure')}>
+          <img
+            id={id}
+            className={cx('image', { zoom: isZoomed })}
+            src={src}
+            alt={caption}
+            style={{
+              transform: isZoomed ? transform : 'none',
+            }}
+          />
+          {caption && <figcaption>{caption}</figcaption>}
+        </figure>
+      </div>
+      {isZoomed && <div className={cx('overlay')} onClick={handleClick} />}
+    </>
   )
 }
