e5c17fadde58ce244f7d678373bea7046bfd99ac
jaepang <jake_king@kakao.com>
2023-05-05 00:28:13 +0900

    next.js fetch test

--- shared/notion/index.ts
+++ shared/notion/index.ts
@@ -3,7 +3,7 @@ import { Client, LogLevel } from '@notionhq/client'
 export const notion = new Client({
   auth: process.env.NOTION_TOKEN,
   //logLevel: LogLevel.DEBUG,
-  //fetch: fetch,
+  fetch: fetch,
 })
 
 export * from './queries'
