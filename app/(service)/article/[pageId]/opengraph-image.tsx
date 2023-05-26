23b823267e44ae45cad4346f507aef9923535dbc
jaepang <jake_king@kakao.com>
2023-05-26 11:14:46 +0900

    og image test

--- app/(service)/article/[pageId]/opengraph-image.tsx
+++ app/(service)/article/[pageId]/opengraph-image.tsx
@@ -29,6 +29,8 @@ export default async function Image({ params }: Props) {
     (
       <div
         style={{
+          width: '100%',
+          height: '100%',
           background: cover ? `url(${cover})` : '#000',
           fontSize: '128',
           display: 'flex',
